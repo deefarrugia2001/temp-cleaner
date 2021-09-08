ad42e480131eb5f96c51afc1db46b6a1b5cd44ba
Mandy Farrugia <87620808+deefarrugia2001@users.noreply.github.com>
2021-09-08 21:52:18 +0200

    Set the path to prefetch.
    this.path for Prefetch points to the Prefetch folder residing in C:\Windows.

--- VSTempCleaner/VSTempCleaner/Prefetch.cs
+++ VSTempCleaner/VSTempCleaner/Prefetch.cs
@@ -1,15 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace VSTempCleaner
+﻿namespace VSTempCleaner
 {
     public class Prefetch : Repository
     {
         public Prefetch() : base() 
         {
+            this.path = @"C:\Windows\prefetch";
         }
     }
 }

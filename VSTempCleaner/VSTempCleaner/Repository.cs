0cc6e6ce9719333c0519e2b6942e07ddaa7ffb1f
Mandy Farrugia <87620808+deefarrugia2001@users.noreply.github.com>
2021-09-08 17:47:11 +0200

    Used summary comments in lieu of multi-line comments.
    Further elaboration regarding path is also present in this commit.

--- VSTempCleaner/VSTempCleaner/Repository.cs
+++ VSTempCleaner/VSTempCleaner/Repository.cs
@@ -8,8 +8,10 @@ namespace VSTempCleaner
 {
     public class Repository
     {
-        /* path will contain the absolute path of the folder containing the temporary files. 
-         * The accessibility modifier has been set to protected as the member needs to be accessed from deriving classes. */
+        /// <summary>
+        /// Contains the absolute path of the folder containing the temporary files. (For instance: C:\Users\[USER]\AppData\Local\Temp).
+        /// The accessibility modifier has been set to protected as the member needs to be accessed from deriving classes.
+        /// </summary>
         protected string path;
     }
 }

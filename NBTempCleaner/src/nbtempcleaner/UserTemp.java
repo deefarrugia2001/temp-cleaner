3511c370b5fc2bbf29a51ff501190d91af68793a
Mandy Farrugia <87620808+deefarrugia2001@users.noreply.github.com>
2021-09-14 21:03:21 +0200

    UserTemp now inherits from Repository.

--- NBTempCleaner/src/nbtempcleaner/UserTemp.java
+++ NBTempCleaner/src/nbtempcleaner/UserTemp.java
@@ -1,5 +1,7 @@
 package nbtempcleaner;
 
-public class UserTemp {
-    
+/* Please note that this only deletes the %temp% content of the currently logged in user!
+ * extends denotes that UserTemp can now access members belonging to Repository. 
+ * final denotes that classes cannot derive from this class, hence it can NEVER be a base class. */
+public final class UserTemp extends Repository {
 }

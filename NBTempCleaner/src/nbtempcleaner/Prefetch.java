b92ecb1b66ed9bea33471560fa96af560732453a
Mandy Farrugia <87620808+deefarrugia2001@users.noreply.github.com>
2021-09-14 20:52:04 +0200

    Initialized path when invoking constructor and set class to final.
    This way, it can never be a base class.

--- NBTempCleaner/src/nbtempcleaner/Prefetch.java
+++ NBTempCleaner/src/nbtempcleaner/Prefetch.java
@@ -1,6 +1,9 @@
 package nbtempcleaner;
 
-//extends denotes that Prefetch can now access members belonging to Repository.
-public class Prefetch extends Repository {
-    
+/* extends denotes that Prefetch can now access members belonging to Repository. 
+ * final denotes that classes cannot derive from this class, hence it can NEVER be a base class. */
+public final class Prefetch extends Repository {
+  public Prefetch() {
+      this.path = "C:\\Windows\\prefetch"; //When instantiating an object of Prefetch, said path will be assigned.
+  }  
 }

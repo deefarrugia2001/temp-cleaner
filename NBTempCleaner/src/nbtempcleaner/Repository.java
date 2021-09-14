a60be6bf2f2e7264cd604d7658d2ae5b5bb11592
Mandy Farrugia <87620808+deefarrugia2001@users.noreply.github.com>
2021-09-14 19:29:34 +0200

    Imported java.io.File and declared a String variable to contain the path to the folders with temporary data.

--- NBTempCleaner/src/nbtempcleaner/Repository.java
+++ NBTempCleaner/src/nbtempcleaner/Repository.java
@@ -1,5 +1,9 @@
 package nbtempcleaner;
 
+/* Imported in order to access and create directories, as well as to fetch 
+ * their respective sub-folders and prooeed with deletion. */
+import java.io.File;
+
 /* Repository has been declared abstract with the aim to simply contain members 
  * common to all classes representing folders containing temporary data. 
  * If one instantiates an object of Repository, the compiler will not know 
@@ -8,5 +12,12 @@ package nbtempcleaner;
  * of type Repository. Upon declaring Repository abstract, one will encounter
  * an error as objects of an abstract type cannot be instantiated. */
 public abstract class Repository {
+    String path; //Contains the absolute path to the folder containing temporary data.
     
+    /* Used a getter to fetch the absolute path. A setter was not defined 
+     * because we do not intend to change the path from the one defined 
+     * in the constructors of the defined classes. */
+    public String getPath() {
+        return this.path; //Return the absolute path (this refers to the current class).
+    }
 }

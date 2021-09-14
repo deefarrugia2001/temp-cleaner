221b6c6ce43784d378ebc3bc0620aaeb69193df2
Mandy Farrugia <87620808+deefarrugia2001@users.noreply.github.com>
2021-09-14 19:20:23 +0200

    Declared Repository as abstract.
    Repository has been declared abstract with the aim to simply contain members common to all classes representing folders containing temporary data. If one instantiates an object of Repository, the compiler will not know which folder to target for deletion. However, it is possible to instantiate an object of a type deriving from Repository and storing it in a variable
    of type Repository. Upon declaring Repository abstract, one will encounter an error as objects of an abstract type cannot be instantiated.

--- NBTempCleaner/src/nbtempcleaner/Repository.java
+++ NBTempCleaner/src/nbtempcleaner/Repository.java
@@ -1,5 +1,12 @@
 package nbtempcleaner;
 
-public class Repository {
+/* Repository has been declared abstract with the aim to simply contain members 
+ * common to all classes representing folders containing temporary data. 
+ * If one instantiates an object of Repository, the compiler will not know 
+ * which folder to target for deletion. However, it is possible to instantiate 
+ * an object of a type deriving from Repository and storing it in a variable 
+ * of type Repository. Upon declaring Repository abstract, one will encounter
+ * an error as objects of an abstract type cannot be instantiated. */
+public abstract class Repository {
     
 }

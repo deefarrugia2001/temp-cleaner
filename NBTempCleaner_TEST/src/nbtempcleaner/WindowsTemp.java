98ab13a0382e9bc7019b99e728ba68b43877aebc
Mandy Farrugia <87620808+deefarrugia2001@users.noreply.github.com>
2021-09-14 21:12:27 +0200

    WindowsTemp now inherits from Repository

--- NBTempCleaner_TEST/src/nbtempcleaner/WindowsTemp.java
+++ NBTempCleaner_TEST/src/nbtempcleaner/WindowsTemp.java
@@ -1,5 +1,7 @@
 package nbtempcleaner;
 
-public class WindowsTemp {
+/* extends denotes that WindowsTemp can now access members belonging to Repository. 
+ * final denotes that classes cannot derive from this class, hence it can NEVER be a base class. */
+public class WindowsTemp extends Repository {
     
 }

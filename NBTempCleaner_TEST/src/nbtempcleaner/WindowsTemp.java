ab613b14fbdc8acde7d8c791f170dfaddf688d82
Mandy Farrugia <87620808+deefarrugia2001@users.noreply.github.com>
2021-09-14 21:14:54 +0200

    WindowsTemp constructor set!

--- NBTempCleaner_TEST/src/nbtempcleaner/WindowsTemp.java
+++ NBTempCleaner_TEST/src/nbtempcleaner/WindowsTemp.java
@@ -3,5 +3,7 @@ package nbtempcleaner;
 /* extends denotes that WindowsTemp can now access members belonging to Repository. 
  * final denotes that classes cannot derive from this class, hence it can NEVER be a base class. */
 public class WindowsTemp extends Repository {
-    
+    public WindowsTemp() {
+        this.path = "C:\\Windows\\Temp";
+    }
 }

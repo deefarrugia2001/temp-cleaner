65f334d84fe98e51b8cdada8a4e3b7aeb1785213
Mandy Farrugia <87620808+deefarrugia2001@users.noreply.github.com>
2021-09-14 21:09:36 +0200

    UserTemp constructor set!

--- NBTempCleaner/src/nbtempcleaner/UserTemp.java
+++ NBTempCleaner/src/nbtempcleaner/UserTemp.java
@@ -1,7 +1,12 @@
 package nbtempcleaner;
 
+import com.sun.security.auth.module.NTSystem;
+
 /* Please note that this only deletes the %temp% content of the currently logged in user!
  * extends denotes that UserTemp can now access members belonging to Repository. 
  * final denotes that classes cannot derive from this class, hence it can NEVER be a base class. */
 public final class UserTemp extends Repository {
+    public UserTemp() {
+        this.path = "C:\\Users\\" + new NTSystem().getName() + "\\AppData\\Local\\Temp";
+    }
 }
